6bd4cdfc3c3b56b6a0e02c6ec81010b72bd9d9f3
Lorenzo Ceccanti <ceccantilorenzo@gmail.com>
2024-01-25 17:22:00 +0100

    Fixed Mongo Connection (no TLS)

--- PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/dao/BaseMongoDAO.java
+++ PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/dao/BaseMongoDAO.java
@@ -8,11 +8,14 @@ import com.mongodb.client.MongoClients;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public abstract class BaseMongoDAO {
 
     private static final String ENV_FILE = ".env";
+    private static final String PEM_FILE = "mongodb.pem";
     private static String envPayload;
     private static MongoClient mongoclient;
 
@@ -30,20 +33,25 @@ public abstract class BaseMongoDAO {
 
     public static MongoClient beginConnection(){
         String[] var = new String[2];
+        ArrayList<String> params = new ArrayList<>();
         retrieveEnv();
         StringTokenizer tokens = new StringTokenizer(envPayload, "\n");
-        var = tokens.nextToken().split("=", 2);
-        String SERVER_ADDRESS = var[1];
-        var = tokens.nextToken().split("=", 2);
-        int MONGO_PORT = Integer.parseInt(var[1].trim());
-        var = tokens.nextToken().split("=", 2);
-        String MONGO_USER = var[1];
-        var = tokens.nextToken().split("=", 2);
-        String MONGO_PASS = var[1];
+        while(tokens.hasMoreTokens())
+        {
+            var = tokens.nextToken().split("=", 2);
+            params.add(var[1]);
+        }
+        String SERVER_ADDRESS = params.get(0).trim();
+        int MONGO_PORT = Integer.parseInt(params.get(1).trim());
+        String MONGO_USER = params.get(2).trim();
+        String MONGO_PASS = params.get(3).trim();
 
-        // Does not connect due to TLS
-        String connectionString = String.format("mongodb://%s:%s@%s:%d/?tls=true&tlsCertificateKeyFile=", MONGO_USER,
-                MONGO_PASS, SERVER_ADDRESS, MONGO_PORT);
+        System.out.print("SERVER_ADDRESS: " + SERVER_ADDRESS);
+        System.out.print(" MONGO_PORT : " + MONGO_PORT);
+        System.out.print(" MONGO USER: " + MONGO_USER);
+        System.out.println(" MONGO_PASS: " + MONGO_PASS);
+        String connectionString = String.format("mongodb://%s:%s@%s:%d/", MONGO_USER, MONGO_PASS, SERVER_ADDRESS,
+                MONGO_PORT);
         mongoclient = MongoClients.create(
                 MongoClientSettings.builder()
                         .applyConnectionString(new ConnectionString(connectionString))
--- PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/dao/UserMongoDAO.java
+++ PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/dao/UserMongoDAO.java
@@ -11,19 +11,25 @@ import org.bson.conversions.Bson;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.mongodb.client.model.Aggregates.match;
+import static com.mongodb.client.model.Filters.eq;
+
 public class UserMongoDAO extends BaseMongoDAO implements UserDAO{
     @Override
     public RegisteredUser makeLogin(String username, String password)
     {
         // Questa è una semplice prova
         MongoClient mongoClient = beginConnection();
+
         MongoDatabase db = mongoClient.getDatabase("pixelindex");
         MongoCollection<Document> usersCollection = db.getCollection("users");
+
         Bson projectionFields = Projections.fields(
                 Projections.include("username"),
                 Projections.excludeId()
         );
-        List<Document> results = usersCollection.find().projection(projectionFields).limit(3).into(new ArrayList<>());
+        Bson myMatch = (eq("username", "maskedgingerjock"));
+        List<Document> results = usersCollection.find(myMatch).into(new ArrayList<>());
         for(int i = 0; i<results.size(); i++)
             System.out.println(results.get(i));
 
