4c49c8fa93d08d4a90076735f56fb47ea1cbd6d3
Lorenzo Ceccanti <ceccantilorenzo@gmail.com>
2024-01-25 23:14:25 +0100

    updated pom

--- PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/Main.java
+++ PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/Main.java
@@ -4,6 +4,7 @@ package it.unipi.largescale.pixelindex;
 
 import it.unipi.largescale.pixelindex.dao.UserMongoDAO;
 import it.unipi.largescale.pixelindex.model.*;
+import it.unipi.largescale.pixelindex.security.Encryptor;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -11,22 +12,26 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate date = LocalDate.parse("1998-02-12", formatter);
+        LocalDate date = LocalDate.parse("1968-10-12", formatter);
         System.out.println("Hello world!");
         System.out.println("myDOB: " + date);
 
         RegisteredUser registered = new RegisteredUser("ITA");
-        Moderator mod = new Moderator("ENG");
-        mod.setName("Lore");
-        mod.setSurname("Cecca");
-        mod.setUsername("ilcecco");
+        registered.setName("Alessandro");
+        registered.setSurname("Ceccanti");
+        /*Moderator mod = new Moderator("ENG");
+        mod.setName("Alessandro");
+        mod.setSurname("Ceccanti");
+        mod.setUsername("ale1968");
         mod.setDateOfBirth(date);
-        mod.setEmail("cecca@lore.it");
-        mod.setHashedPassword("bau");
+        mod.setEmail("cecca2@live.it");*/
+
+        mod.setHashedPassword(Encryptor.hashPassword("pippo"));
         mod.setId("A001");
 
         User u = mod;
         System.out.println(u.toString());
+
 
         // Prova enumerato
         Review r = new Review();
@@ -38,7 +43,9 @@ public class Main {
         // Prova connessione MONGO remoto
         UserMongoDAO uMongoDao = new UserMongoDAO();
 
+
         User u = new RegisteredUser();
+
 
         uMongoDao.makeLogin("maskedgingerjock", "ciao");
 
