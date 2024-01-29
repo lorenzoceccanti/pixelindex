4baf5a9adfaa26647e47b47823c2f16b643c9696
Lorenzo Ceccanti <ceccantilorenzo@gmail.com>
2024-01-29 23:03:31 +0100

    UserSearchDTO: created class with setters and getters

---
+++ PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/dto/UserSearchDTO.java
@@ -0,0 +1,31 @@
+package it.unipi.largescale.pixelindex.dto;
+
+public class UserSearchDTO {
+    private String username;
+    private int countFollowed;
+    private int countFollower;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getCountFollowed() {
+        return countFollowed;
+    }
+
+    public void setCountFollowed(int countFollowed) {
+        this.countFollowed = countFollowed;
+    }
+
+    public int getCountFollower() {
+        return countFollower;
+    }
+
+    public void setCountFollower(int countFollower) {
+        this.countFollower = countFollower;
+    }
+}
