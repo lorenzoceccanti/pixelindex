b0df1345eefd87bb17086799c7573dee0020aea6
Niccolò <niccolo.mule@gmail.com>
2024-01-31 13:10:51 +0100

    Added interfaces for previous push

--- PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/service/LibraryService.java
+++ PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/service/LibraryService.java
@@ -3,7 +3,10 @@ package it.unipi.largescale.pixelindex.service;
 import it.unipi.largescale.pixelindex.dto.GameDTO;
 import it.unipi.largescale.pixelindex.exceptions.ConnectionException;
 
+import java.util.List;
+
 public interface LibraryService {
-    void addGameToLibrary(String username, GameDTO game) throws ConnectionException;
-    void removeGameFromLibrary(String username, GameDTO gameId) throws ConnectionException;
+    void addGame(String username, GameDTO game) throws ConnectionException;
+    void removeGame(String username, GameDTO gameId) throws ConnectionException;
+    List<GameDTO> getGames(String username) throws ConnectionException;
 }
--- PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/service/WishlistService.java
+++ PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/service/WishlistService.java
@@ -3,7 +3,10 @@ package it.unipi.largescale.pixelindex.service;
 import it.unipi.largescale.pixelindex.dto.GameDTO;
 import it.unipi.largescale.pixelindex.exceptions.ConnectionException;
 
+import java.util.List;
+
 public interface WishlistService {
-    void addGameToWishlist(String userId, GameDTO game) throws ConnectionException;
-    void removeGameFromWishlist(String userId, GameDTO gameId) throws ConnectionException;
+    void addGame(String userId, GameDTO game) throws ConnectionException;
+    void removeGame(String userId, GameDTO gameId) throws ConnectionException;
+    List<GameDTO> getGames(String userId) throws ConnectionException;
 }
