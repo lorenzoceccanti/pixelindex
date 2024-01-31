25a2c2ddde47378c473cb664b7cebb2cc14fc814
Lorenzo Ceccanti <ceccantilorenzo@gmail.com>
2024-01-31 17:17:49 +0100

    misc: added query mongodb

---
+++ _queries/queries_mongo.js
@@ -0,0 +1,14 @@
+/* CRUD OPERATION: Inserimento di una nuova segnalazione con il criterio concordato */
+/* Sostituire ale1968 con l'utente incriminato e Chang Liu con l'utente che segnala*/
+db.users.updateOne(
+  {
+    username: "ale1968",
+    $or: [
+      {$expr: {$lt: [{ $size: "$reported_by" }, 5] } },
+      { "reported_by": { $exists: false } }
+    ]
+  },
+  {
+    $addToSet: { reported_by: "Chang Liu" }
+  }
+)
