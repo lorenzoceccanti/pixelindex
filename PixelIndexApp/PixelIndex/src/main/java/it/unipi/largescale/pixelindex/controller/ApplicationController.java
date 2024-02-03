7a54c01625ba4c1de1c11f6e0ff1469c4afda171
Lorenzo Ceccanti <ceccantilorenzo@gmail.com>
2024-02-03 13:44:15 +0100

    Application Controller calling the CLI

--- PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/controller/ApplicationController.java
+++ PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/controller/ApplicationController.java
@@ -1,6 +1,9 @@
 package it.unipi.largescale.pixelindex.controller;
 
+import it.unipi.largescale.pixelindex.dto.AuthUserDTO;
 import it.unipi.largescale.pixelindex.exceptions.ConnectionException;
+import it.unipi.largescale.pixelindex.exceptions.UserNotFoundException;
+import it.unipi.largescale.pixelindex.exceptions.WrongPasswordException;
 import it.unipi.largescale.pixelindex.service.GameService;
 import it.unipi.largescale.pixelindex.service.ModeratorService;
 import it.unipi.largescale.pixelindex.service.RegisteredUserService;
@@ -10,14 +13,37 @@ import it.unipi.largescale.pixelindex.view.options.UnregisteredMenu;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ApplicationController {
+    String username;
+    String password;
+    private void showLogin(RegisteredUserService userService) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Username?");
+        username = sc.nextLine();
+        System.out.println("Password?");
+        password = sc.nextLine();
 
-    private void showLogin() {
-        System.out.println("Insert your login data");
-        // Scanner sc = new Scanner(System)
+        try{
+            AuthUserDTO authUserDTO = null;
+            authUserDTO = userService.makeLogin(username, password);
+            System.out.println("Welcome " + authUserDTO.getName());
+            System.out.println("Your personal information:");
+            System.out.println(authUserDTO);
+        }catch(ConnectionException ex)
+        {
+            System.out.println(ex.getMessage());
+        } catch(UserNotFoundException ex)
+        {
+            System.out.println("User name does not exists");
+        }
+        catch(WrongPasswordException ex)
+        {
+            System.out.println("Wrong password!");
+        }
     }
 
     public ApplicationController() {
@@ -30,29 +56,20 @@ public class ApplicationController {
         Map<Integer, Runnable> hanlderMap = new HashMap<>();
         RegisteredUserService userService = ServiceLocator.getRegisteredUserService();
         Runnable[] functionsArray = new Runnable[]{
-                //  showLogin,
-
+                () -> showLogin(userService)
         };
         functionsArray[index].run();
-        // int[] n = new int[2]
-        /*for (int i = 0; i < unregisteredMenu.optionNumber(); i++) {
-            handlerMap.put(i, )
-        }*/
 
-        /*switch (index) {
-            case 0:
 
-        }*/
-        // int notRegisteredSel = notRegisteredSel.askUserInteraction("unregistered_menu");
-
-        // notRegisteredSel.askUserInteraction();
-
+        /*
         BlockingQueue<Runnable> taskQueue = new LinkedBlockingQueue<>();
         ConsistencyThread consistencyThread = new ConsistencyThread(taskQueue);
         consistencyThread.start();
         GameService gameService = ServiceLocator.getGameService();
+         */
 
         // Prova inserimento gioco con consistency
+
         /*
         Game game = new Game();
         game.setName("GREZZO 3");
@@ -76,9 +93,10 @@ public class ApplicationController {
         } catch(ConnectionException e) {
             System.out.println("Document DB insertion FAILED");
         }
-        */
+
 
         // Prova ban in eventual consistency
+
         ModeratorService moderatorService = ServiceLocator.getModeratorService();
         try {
             moderatorService.banUser("ale1968");
@@ -101,6 +119,6 @@ public class ApplicationController {
         // utente conferma roba
 
         // parte il servizio confermato
-
+        */
     }
 }
