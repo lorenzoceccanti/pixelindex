672eea047e7a3b519b01bdfb9fdba1b4026a2162
CristianoCorsi <c.corsi99@gmail.com>
2024-02-03 20:36:02 +0100

    Started to build the Reaction function (like / dislike)

--- PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/dao/neo4j/ReviewNeo4jDAO.java
+++ PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/dao/neo4j/ReviewNeo4jDAO.java
@@ -1,6 +1,7 @@
 package it.unipi.largescale.pixelindex.dao.neo4j;
 
 import it.unipi.largescale.pixelindex.exceptions.DAOException;
+import it.unipi.largescale.pixelindex.model.Reaction;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Session;
 
@@ -9,6 +10,7 @@ import java.util.Map;
 
 public class ReviewNeo4jDAO extends BaseNeo4jDAO {
 
+    // TODO: test this method
     public void insertReview(String reviewId, String gameId, String author, String rating, String text, String timestamp) throws DAOException {
         try (Driver neoDriver = beginConnection()) {
             String query = """
@@ -44,6 +46,7 @@ public class ReviewNeo4jDAO extends BaseNeo4jDAO {
         }
     }
 
+    // TODO: test this method
     public void removeReview(String reviewId) throws DAOException {
         try (Driver neoDriver = beginConnection()) {
             String query = """
@@ -52,6 +55,29 @@ public class ReviewNeo4jDAO extends BaseNeo4jDAO {
                     """;
             Map<String, Object> params = new HashMap<>();
             params.put("reviewId", reviewId);
+
+            try (Session session = neoDriver.session()) {
+                session.executeWrite(tx -> {
+                    tx.run(query, params);
+                    return null;
+                });
+            }
+        } catch (Exception ex) {
+            throw new DAOException(ex);
+        }
+    }
+
+    public void addReaction(String reviewId, String username, Reaction reaction) throws DAOException {
+        // TODO: QUERY DA RIFARE / FINIRE
+        try (Driver neoDriver = beginConnection()) {
+            String query = """
+                    MATCH (review:Review {mongoId: $reviewId}), (user:User {username: $username})
+                    WHERE NOT EXISTS {(user)-[:LIKES {value: $value}]->(review)}
+                    """;
+            Map<String, Object> params = new HashMap<>();
+            params.put("reviewId", reviewId);
+            params.put("username", username);
+            params.put("value", reaction == Reaction.LIKE);
 
             try (Session session = neoDriver.session()) {
                 session.executeWrite(tx -> {
