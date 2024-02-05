8c6840957660de3d6ee836ef4938f1d8fe68d591
CristianoCorsi <c.corsi99@gmail.com>
2024-02-05 17:19:14 +0100

    Aggiunto ReviewPageDTO per gestire la singola pagina di recensioni

---
+++ PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/dto/ReviewPageDTO.java
@@ -0,0 +1,24 @@
+package it.unipi.largescale.pixelindex.dto;
+
+import java.util.List;
+
+public class ReviewPageDTO {
+    Integer totalReviewsCount;
+    List<ReviewPreviewDTO> reviews;
+
+    public Integer getTotalReviewsCount() {
+        return totalReviewsCount;
+    }
+
+    public List<ReviewPreviewDTO> getReviews() {
+        return reviews;
+    }
+
+    public void setTotalReviewsCount(Integer totalReviewsCount) {
+        this.totalReviewsCount = totalReviewsCount;
+    }
+
+    public void setReviews(List<ReviewPreviewDTO> reviews) {
+        this.reviews = reviews;
+    }
+}
