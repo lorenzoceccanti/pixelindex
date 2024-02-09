89ae99cf47b34a5c2f938b3f6e529b6e99a9d568
Lorenzo Ceccanti <ceccantilorenzo@gmail.com>
2024-02-09 18:11:05 +0100

    misc

--- _queries/queries_eventual.js
+++ _queries/queries_eventual.js
@@ -1,11 +1,24 @@
 /* Query che setta su mongo i seguiti di A e seguaci di B*/
-db.prova.updateMany(
+
+  db.users.updateMany(
     { $or: [ { username: "maskedgingerjock" }, { username: "MisterEcho" } ] },
     [
       {
         $set: {
-          followers: { $cond: { if: { $eq: [ "$username", "MisterEcho" ] }, then: 33, else: "$followers" } }, /* nuovi seguaci di B*/
-          following: { $cond: { if: { $eq: [ "$username", "maskedgingerjock" ] }, then: 30, else: "$following" } } /* nuovi seguiti di A*/
+          followers: {
+            $cond: { 
+              if: { $eq: [ "$username", "MisterEcho" ] }, 
+              then: 33, 
+              else: "$followers"
+            }
+          },
+          following: {
+            $cond: { 
+              if: { $eq: [ "$username", "maskedgingerjock" ] }, 
+              then: 30, 
+              else: "$following"
+            }
+          }
         }
       }
     ]
