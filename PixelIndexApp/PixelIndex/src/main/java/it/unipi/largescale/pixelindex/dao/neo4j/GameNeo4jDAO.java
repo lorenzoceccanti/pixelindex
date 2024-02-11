194441f978480f654b08a7705a7743fabc903e2a
CristianoCorsi <c.corsi99@gmail.com>
2024-02-11 20:10:01 +0100

    Added getMostAddedToLibraryGames

--- PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/dao/neo4j/GameNeo4jDAO.java
+++ PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/dao/neo4j/GameNeo4jDAO.java
@@ -1,10 +1,13 @@
 package it.unipi.largescale.pixelindex.dao.neo4j;
 
+import it.unipi.largescale.pixelindex.dto.TrendingGamesDTO;
 import it.unipi.largescale.pixelindex.exceptions.DAOException;
 
 import it.unipi.largescale.pixelindex.model.Game;
 import org.neo4j.driver.Session;
 import org.neo4j.driver.Driver;
+
+import java.util.List;
 
 import static org.neo4j.driver.Values.parameters;
 
@@ -19,6 +22,31 @@ public class GameNeo4jDAO extends BaseNeo4jDAO {
                             "name", game.getName(),
                             "releaseYear", game.getReleaseDate().getYear()));
                     return null;
+                });
+            }
+        } catch (Exception ex) {
+            throw new DAOException(ex);
+        }
+    }
+
+    public List<TrendingGamesDTO> getMostAddedToLibraryGames(Integer year, Integer limit) throws DAOException {
+        try (Driver neoDriver = beginConnection()) {
+            String query = """
+                    MATCH (g:Game)
+                    WHERE g.releaseYear = $year
+                    RETURN g.mongoId AS id, g.name AS name, g.releaseYear AS releaseYear
+                    ORDER BY g.mongoId
+                    LIMIT $limit
+                    """;
+            try (Session session = neoDriver.session()) {
+                return session.executeRead(tx -> {
+                    return tx.run(query, parameters("year", year, "limit", limit))
+                            .list(record -> {
+                                TrendingGamesDTO game = new TrendingGamesDTO();
+                                game.setGameName(record.get("name").asString());
+                                game.setCount(record.get("releaseYear").asInt());
+                                return game;
+                            });
                 });
             }
         } catch (Exception ex) {
---
+++ PixelIndexApp/PixelIndex/src/main/java/it/unipi/largescale/pixelindex/dto/TrendingGamesDTO.java
@@ -0,0 +1,26 @@
+package it.unipi.largescale.pixelindex.dto;
+
+public class TrendingGamesDTO {
+    private String gameName;
+    private Integer count;
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String toString() {
+        return gameName + " " + count;
+    }
+}
